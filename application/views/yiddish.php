560ad85df237299aeddad3cc2b54828a15ba0ea8
Maor Kern <maor.kern@gmail.com>
2016-06-15 23:59:14 +0300

    d

---
+++ application/views/yiddish.php
@@ -0,0 +1,345 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>חבריא</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Dosis:300,400,500,600,700">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.2.0-rc2/css/bootstrap-rtl.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/owl-carousel/owl.carousel.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/elegant-icons/css/elegant-icons.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/owl-carousel/owl.carousel.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/owl-carousel/owl.theme.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/cubeportfolio/css/cubeportfolio.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
+<link rel="icon" href="assets/img/topicon.png">
+            <!--Colors-->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/red.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/orange.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/green.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/lime.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/brown.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/blue.css"> -->
+            <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/yellow.css"> -->
+             <!-- <link rel="stylesheet" type="text/css" href="assets/css/colors/purple.css"> -->
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+
+    </head>
+
+    <body data-spy="scroll" data-offset="80">
+         <div class="navbar navbar-default navbar-fixed-top menu-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#home">היים בלאט</a></li>
+                        <li><a href="#about-us-section">וועגן אונז</a></li>
+                        <li><a href="#benefits-section">אינפארמאציע</a></li>
+                        <li><a href="#services-section">אינזערע סערוויסעס</a></li>
+                        <li><a href="#contact-section">איינשרייבן</a></li>
+                        <li><a href="#callaction-container">שטיצט די פראיעקט</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!-- Top content -->
+        <div id="home" class="parallax top-content" data-stellar-background-ratio="0.3">
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+
+			<div class="col-md-10 col-md-offset-1 top-description">
+
+				<h2 class="text-center">
+				<img src="../assets/img/logo.png" width="350px" /></h2>
+
+
+		<p>
+'חבריא' האט געמאכט פאר זיך א ציל, בראש ובראשונה צוזאמנעמען א גרופע חסידישע משפחות פון די וואס קומען ארויף וואוינען קיין ארץ ישראל און אויפשטעלן א געשמאקע קהילה, קהילה נאה וחסודה, אין א שיינע פאסיגע פלאץ, מיט פאסיגע מוסדות חינוך פאר די קינדערלעך.</p>
+		                            <div class="top-button">
+                            	   <a class="btn btn-top-white btn-lg" href="#about-us-section">לחץ ללמוד עוד <i class="icon ion-mouse" style="margin-right:2px"></i>  </a>
+                                 <a class="btn btn-top-white btn-lg" href="<?= base_url() ?>">Switch to English</a>
+                                 <a class="btn btn-top-white btn-lg" href="<?= base_url() ?>index.php/he">החלף לעברית</a>
+
+                            </div>
+
+					</div>
+                            </div>
+
+					</div>
+
+
+				</div>
+			</div>
+
+        <!-- Features -->
+
+<div id="about-us-section" class="product-container product-bg2">
+            <div class="container" style="text-align:right">
+                <div class="row">
+
+                <div class="col-lg-6 col-sm-6">
+                    <div class="productbox" >
+                        <div>
+                            <i class="icon ion-information"></i>
+                        </div>
+                        <h5>חבריא</h5>
+                        איז א גרופע היימישע משפחות פון די פאראייניגטע שטאטן און אלעוועלטליך, וואס ווילן - יעדע משפחה פאר זייערע סיבות - ארויפקומען וואוינען צום הייליגן לאנד ארץ ישראל. חבריא וועט בעז״ה שטיין צו הילף פאר אלע וואס קומען ארויף פון חו״ל.
+                    </div>
+
+                    <div class="productbox">
+                        <div>
+                            <i class="icon ion-android-expand"></i>
+                        </div>
+                        <h5>חבריא</h5>
+                        איז גרייט בעזרת השם זיך צו פארברייטערן, כדי זיך צו אראפ שטעלן אלץ די ריכטיגע אדרעס פאר ישיבה בחורים און סעמינאר מיידלעך וואס קומען אליינס לערנען קיין ארץ ישראל, זיי ארויסצוהעלפן מיט וואס שייך, ברוחניות ובגשמיות.
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="productbox">
+                        <div>
+                            <i class="icon ion-android-archive"></i>
+                        </div>
+                        <h5>חבריא</h5>
+                        האט אויף זיך גענומען צו עול צו זיין זיין די רעכטע האנט פאר די גרופע, די שטאב מיטגלידער ארבעטן שוין עטליכע יאר מיט די ריכטיגע מענטשן מיט יעדער וואס קען העלפן, כדי גרינגער צו מאכן ווי ווייט מעגליך אויף די מופן פראצעדור און וואס עס ברענגט מיט ווי למשל, טרעפן א פלאץ ווי צו וואוינען, שאפן די ריכטיגע פרנסה, איינשרייבן די קינדער אין די ארטיגע מוסדות חינוך ווי אויך אויפשטעלן א בית המדרש און א בא׳טעמ׳טע קהילה צענטער וואס וועט בעז״ה יעדן פאראייניגן ווי איין גרויסע משפחה.
+                    </div>
+                    <div class="productbox">
+                        <div>
+                            <i class="icon ion-android-plane"></i>
+                        </div>
+                        <h5>חבריא</h5>
+                        העלפט אויך פאר אנדערע גרופעס אדער יחידים, היימישע אידן וואס וועלן אויסוועלן צו וואוינען אין אנדערע פלעצער אין לאנד, און שטרעקט אויס איר האנט אויך פאר זיי, אז אויך זיי זאלן קענען באקומען די הילף און אינפארמאציע גרינגער צו מאכן דאס מופן.
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
+        <div id="benefits-section" class="features-container benefits-container" style="padding-bottom:0">
+            <div class="inner-bg benefits-inner-container">
+            <div class="container ">
+                <div class="row">
+                    <div class="col-sm-12 features">
+                        <h2>דע לך</h2>
+                        <h4 style="color: white; line-height: 200%;">מופען אין א פרעמד לאנד איז נישט קיין גרינגע זאך, מען דארף קענען דעם הלך יילך, פארשטיין די ארטיגע שפראך. עס קען נעמען א שטיק צייט ביז ווען מען ווערט געהעריג איינגעגלידערט אין די סביבה. אין ארץ ישראל איז דא גיטע פראגראמען וואס זענען באקאנט אלץ איינע פון די בעסטע אין דער וועלט פאר נייע איינוואוינער, ספעציעל פאר די וואס קומען ארויף פון אמעריקא. די געטרייע "חבריא" שטאב העלפן ארויס די היימישע אידן צו באקומען דאס בעסטע און אויפן גרינגסטען אויפן וואס מעגליך.
+עס איז פארהאן פארשידענע ערליי בענעפיטן ווי למשל רייזע בילעטן בחינם, סובסודיעס אין די רענט פרייזן די ערשטע פאר יאר, הילף און ארויסנעמען א מארטגעדש, אַ קורס צו לערנען די היגע שפראך אן אפצאל, געלט צו שטודירן העכער בילדונג אדער א פאך, הנחות אין שטייערן אויף ארויפברענגען אדער קויפן א נייע אויטא, הנחות אין הכנסה שטייערן, בעיביסטינג און פלעי-גרופ הילף ווי אויך א גרויסע סכום געלט פער מענטש וואס ווערט געגעבן גלייך ביים אנקומען אין לאנד.
+די איינגעשריבענע צו "חבריא" וועלן באַקומען די פולע אינפורמאציע אויף די בענעפיטן, יעדע בית אב לויט די צאל קינדער אין די פאמיליע און זייער עלטער</h4>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        </div>
+
+        <!-- Product -->
+        <div id="services-section" class="features-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 features">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4 features-box1">
+                        <div class="icon">
+                            <i class="icon ion-ios-body"></i>
+                        </div>
+                        <h4>הילף צו מופן</h4>
+                        <p>
+                            חבריא - ארבעט אינאיינעם מיט מערערע ארגאנאזאציעס און שטאטישע ביורא באאמטע די לעצטערע יארן און פרובירט צו טון וואס אימער גרינגער צו מאכן פאר יעדן יחיד ארויפצוקומען וואוינען קיין ארץ ישראל מיט וואספארא הילף עס נאר נויטיג.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 features-box2">
+                        <div class="icon">
+                            <i class="icon ion-android-home"></i>
+                        </div>
+                        <h4>וואוינארט</h4>
+                        <p>
+                            חבריא - שטעלט צוזאם א גרופע צו איינהאנדלען דירות אינאיינעם אין די קומענדיגע פאר מאנאטן, כדי זיך צו באזעצן אין איין געגענט. דערווייל העלפט חבריא פאר די משפחות וואס קומען ארויף צו טרעפן א פאסיגע דירה צו דינגען גרינגערהייט אין מערערע געגענטער.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 features-box3">
+                        <div class="icon">
+                            <i class="icon ion-android-contacts"></i>
+                        </div>
+                        <h4>קהילה</h4>
+                        <p>
+                          די ערשטע קהילה פאר היימישע חסידישע משפחות וואס קומען ארויף צום הייליגע לאנד וועט בעז״ה אויפגעשטעלט אויף די בערג ארום ירושלים עיה״ק, א ווארעמע פאראייניגטע קהילה און וועט צושטעלן אלע קהל׳ישע געברויכן מיטן חסידישע געשמאק - א קהילה צענטער, מקווה טהרה, אוצר הספרים, א רחבת'דיגע ליפטיגע בית מדרש צו דאווענען און לערנען און פראגראמען פאר די אינגווארג.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 features-box4">
+                        <div class="icon">
+                            <i class="icon ion-settings"></i>
+                        </div>
+                        <h4>פרנסה</h4>
+                        <p>
+                            חבריא - איז עוסק אין שאפן די ריכטיגע ארבייט שטעלעס פאר מענער און די בני בית צוגלייך, די הנהלה איז בקשר מיט פארשידענע היימישע ארבייטסגעבער און קאמערציאלע פירמעס אין לאנד און אין אויסלאנד, חבריא איז משתדל צו טרעפן די פאסיגע מקור פרנסה פאר יעדן יחיד.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 features-box5">
+                        <div class="icon">
+                            <i class="icon ion-ios-book"></i>
+                        </div>
+                        <h4>חינוך</h4>
+                        <p>
+                            חבריא - העלפט ארויס די עלטערן מיטן איינשרייבן די קינדער אין מוסדות חינוך וואס זענען פאסיג פאר נייע איינוואוינער, מיט די צייט וועט 'חבריא' בעז״ה אויפשטעלן אייגענע מוסדות חינוך אין ראם פון די נייע קהילה.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 features-box6">
+                        <div class="icon">
+                            <i class="icon ion-levels"></i>
+                        </div>
+                        <h4>הכנה לעליה</h4>
+                        <p>
+                            חבריא מושיטה את ידיה לעזרת המשפחות עוד בטרם יציאתם לדרך. הן במישור הציבורי - ע"י עריכת כינוסים ואירועים מיוחדים במטרה להפגיש בין המשפחות, איש את רעהו יעזורו ולאחיו יאמר חזק. והן במישור הפרטי - ע"י נתינת מענה לכל שאלה בניסיון להסיר כל מכשול ובעיה בדרך לעליה.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- Callaction -->
+
+        <div id="callaction-container" class="callaction-container">
+            <div class="inner-callaction">
+	        <div class="container">
+            <div class="row">
+                <h2>שטיצט די פראיעקט</h2>
+                <h3>צריכים את עזרתך</h3>
+    <h4>בתרומתך ל"חבריא" הנך זוכה לקחת חלק בפעילות האדירה למען העולים לארץ ישראל, ולכבד את ה' מהונך על ידי נתינת צדקה להחזקת יושבי הארץ - הלא היא צדקת רבי מאיר בעל הנס - אשר כל צדיקי הדורות קדשוה ושבחוה, ואשר על ידה מתברכים התורמים בשפע רב טוב מן השמים. </h4> <br>
+    <h4>אם ברצונכם לתרום, נא שלחו אימייל לcontact@chavraya.com</h4>
+<!--<a href="#" class="btn btn-top-white btn-lg" style="">Call To Action</a>-->
+</div>
+</div>
+</div>
+</div>
+
+        <!-- Pricing -->
+
+
+
+
+
+                <!-- Testimonials -->
+
+
+        <!-- Contact us -->
+        <div id="contact-section" class="contact-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 contact">
+                    <h2 style="color:black">איינשרייבן</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 form-group register-a-form">
+
+                    <form method="post" action="<?= base_url() ?>index.php/main/register_user" id="register_form">
+                        <input type="text" name="name" placeholder="נאמען" class="form-control contact-name">
+                        <input type="text" name="city" placeholder="שטאט" class="form-control contact-city">
+                        <input type="text" name="country" placeholder="לאנד" class="form-control contact-city">
+                        <input type="text" name="email" placeholder="אימייל" class="form-control contact-email">
+                        <input type="text" name="phone" placeholder="טעל" class="form-control contact-phone-number">
+                        <input type="text" name="family-size" placeholder="קינדער" class="form-control contact-fam-size">
+                        <textarea name="message" class="form-control" placeholder="הערה"></textarea>
+                        <button type="submit" class="btn btn-contact" id="dat" onclick="submit_form()">הירשם</button>
+                    </form>
+
+                </div>
+
+            </div>
+          </div>
+        </div>
+
+
+	<!-- footer -->
+	<div id="footer">
+	<h3>עקבו אחרינו!</h3>
+	<div class="follow-us">
+                        <a href="https://www.facebook.com/chavraya" class="social-icon"><i class="fa fa-facebook"></i></a>
+                        <a href="https://twitter.com/thechavraya" class="social-icon"><i class="fa fa-twitter"></i></a>
+                        <a href="https://instagram.com/chavraya" class="social-icon"><i class="fa fa-instagram"></i></a>
+                        <a href="https://youtube.com" class="social-icon" disabled><i class="fa fa-youtube"></i></a>
+		</div>
+	</div>
+	<!-- footer 2 -->
+	<div id="footer2">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+				<div class="copyright">&copy;Chavraya
+							<script type="text/javascript">
+							//<![CDATA[
+								var d = new Date()
+								document.write(d.getFullYear())
+								//]]>
+								</script>
+
+						</div>
+						</div>
+					</div>
+				</div>
+					</div>
+
+        <!-- Javascript -->
+        <script src="<?= base_url() ?>assets/js/jquery-1.10.2.min.js"></script>
+        <script src="<?= base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?= base_url() ?>assets/js/retina-1.1.0.min.js"></script>
+        <script src="<?= base_url() ?>assets/js/jquery.easing.1.3.min.js"></script>
+        <script src="<?= base_url() ?>assets/js/jquery.stellar.min.js" type="text/javascript"></script>
+        <script src="<?= base_url() ?>assets/owl-carousel/owl.carousel.min.js"></script>
+        <script src="<?= base_url() ?>assets/cubeportfolio/js/jquery.cubeportfolio.min.js"></script>
+        <script src="<?= base_url() ?>assets/js/cube.js"></script>
+        <script src="<?= base_url() ?>assets/js/script.js"></script>
+        <script src="<?= base_url() ?>assets/js/main.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+        <script>
+        <?php if($did_register == true): ?>
+        $(document).ready(function(){
+            swal("תודה!", "נרשמת בהצלחה", "success");
+
+  });
+
+        <?php endif ?>
+        function submit_form(){
+            //do validation
+            $("#dat").submit();
+        }
+        </script>
+
+    </body>
+
+</html>
